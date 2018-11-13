2b6875927689257e1057631339d7c440e9798029
khiemcaokms <43228818+khiemcaokms@users.noreply.github.com>
2018-11-14 01:04:40 +0700

    fix-feed-data
    change the flow to feed data

--- src/main/java/ExcelTemplate.java
+++ src/main/java/ExcelTemplate.java
@@ -1,44 +1,42 @@
 import java.io.IOException;
-import java.util.Date;
 
 public class ExcelTemplate {
 
-    public void setHeader(String src, String sheet, int column) throws IOException {
-        String reportDate = ExcelDriven.getCellValue(src, "Dashboard", 1, 1);
-
-        ExcelDriven.setCellValue(src, sheet, 0, column, "Date:");
-        ExcelDriven.setCellValue(src, sheet, 0, column+1, reportDate);
-        ExcelDriven.setCellValue(src, sheet, 1, column, "Status");
-        ExcelDriven.setCellValue(src, sheet, 1, column+1, "Note");
-
-        int totalRow = ExcelDriven.getTotalRow(src, sheet);
-        for (int i = 2; i <= totalRow; i++){
-            ExcelDriven.setCellValue(src, sheet, i, column, "Not Run on Sauce Lab");
+    public void setHeader(String src, String DetailInfo, int column, String reportDate) throws IOException {
+        //create a template for the new report
+        ExcelDriven.setCellValue(src, DetailInfo, 0, column, "Date:");
+        ExcelDriven.setCellValue(src, DetailInfo, 0, column+1, reportDate);
+        ExcelDriven.setCellValue(src, DetailInfo, 1, column, "Status");
+        ExcelDriven.setCellValue(src, DetailInfo, 1, column+1, "Note");
+        //create a default value
+        for (int i = 2; i <= ExcelDriven.getTotalRow(src, DetailInfo); i++){
+            ExcelDriven.setCellValue(src, DetailInfo, i, column, "Not Run on Sauce Lab");
         }
     }
 
-    public void addToDataTable(String src, String sheet) throws IOException{
-        String reportName = "[" + ExcelDriven.getCellValue(src, "Dashboard", 0, 1)
-                + "] " + ExcelDriven.getCellValue(src, "Dashboard", 1, 1);
+    public void addToDataTable(String src, String DataTable, String reportName) throws IOException{
 
         for (int i=1; i<=10; i++){
-            if (ExcelDriven.getCellValue(src, "Chart Data", i, 0) == ""){
-                ExcelDriven.setCellValue(src, "Chart Data", i, 0, reportName);
+            if (ExcelDriven.getCellValue(src, DataTable, i, 0) == "" || ExcelDriven.getCellValue(src, DataTable, i, 0) == null){
+                //add report date to data table
+                ExcelDriven.setCellValue(src, DataTable, i, 0, reportName);
 
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 1, "COUNTIF('Detail Info'!D:D, \"Passed on Sauce Lab\")");
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 2, "COUNTIF('Detail Info'!D:D, \"Passed After Re-Running\")");
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 3, "COUNTIF('Detail Info'!D:D, \"Failed By Bug\")");
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 4, "COUNTIF('Detail Info'!D:D, \"Failed By Script\")");
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 5, "COUNTIF('Detail Info'!D:D, \"Not Run on Sauce Lab\")");
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 6, "COUNTA('Detail Info'!C2:C1000)");
+                //feed data from Detail Info sheet
+                ExcelDriven.setCellFormula(src, DataTable, i, 1, "COUNTIF('Detail Info'!D:D, \"Passed on Sauce Lab\")");
+                ExcelDriven.setCellFormula(src, DataTable, i, 2, "COUNTIF('Detail Info'!D:D, \"Passed After Re-Running\")");
+                ExcelDriven.setCellFormula(src, DataTable, i, 3, "COUNTIF('Detail Info'!D:D, \"Failed By Bug\")");
+                ExcelDriven.setCellFormula(src, DataTable, i, 4, "COUNTIF('Detail Info'!D:D, \"Failed By Script\")");
+                ExcelDriven.setCellFormula(src, DataTable, i, 5, "COUNTIF('Detail Info'!D:D, \"Not Run on Sauce Lab\")");
+                ExcelDriven.setCellFormula(src, DataTable, i, 6, "COUNTA('Detail Info'!C:C)-2");
 
-                int j = i+1;
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 8, "B"+j+"/G"+j);
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 9, "C"+j+"/G"+j);
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 10, "D"+j+"/G"+j);
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 11, "E"+j+"/G"+j);
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 12, "F"+j+"/G"+j);
-                ExcelDriven.setCellFormula(src, "Chart Data", i, 13, "SUM(I"+j+":M"+j+")");
+                //generate the percentage number
+                int row = i+1;
+                ExcelDriven.setCellFormula(src, DataTable, i, 8, "B"+row+"/G"+row);
+                ExcelDriven.setCellFormula(src, DataTable, i, 9, "C"+row+"/G"+row);
+                ExcelDriven.setCellFormula(src, DataTable, i, 10, "D"+row+"/G"+row);
+                ExcelDriven.setCellFormula(src, DataTable, i, 11, "E"+row+"/G"+row);
+                ExcelDriven.setCellFormula(src, DataTable, i, 12, "F"+row+"/G"+row);
+                ExcelDriven.setCellFormula(src, DataTable, i, 13, "SUM(I"+row+":M"+row+")");
 
                 return;
             }
